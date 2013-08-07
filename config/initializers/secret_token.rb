54f250c884989ec5173d44f5095ac3312b551415
Alicia Tan <aliciaht@gmail.com>
2013-08-07 12:05:50 -0400

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandbundler::Application.config.secret_token = '16d9fa90a1b50213c2dd7f73d5cadc5c2e645257cc0a511bb30f6bf787a2aab4a42720289bd98ac5186e820b17bcc70ea9a33fbec5c72f9182f61b303a6bd18a'
